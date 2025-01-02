b3bc0b758d5d28f13b14dc062345a352b56a17ed
gacorprjkt-admin <you@example.com>
2025-01-02 22:22:14 +0800

    ci: update
    ada yang marah

--- ci.sh
+++ ci.sh
@@ -31,24 +31,25 @@ export FLAGS="
   CMAKE_STATIC_LINKER_FLAGS='-O3 --lto-O3 --lto-CGO3 --gc-sections --strip-debug'
   "
 
-export FINAL=false
 if [[ $1 == "final" ]]; then
     export FINAL=true
+else
+    export FINAL=false
 fi
 
 if $FINAL; then
-    STATUS="(FINAL)"
+    STATUS="FINAL"
     ADD="--final"
 else
-    STATUS="(PROFILING)"
+    STATUS="PROF"
 fi
 
 send_info() {
     curl -s -X POST https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage \
         -d chat_id="${CHAT_ID}" \
         -d "parse_mode=html" \
-        -d text="<b>${1}</b><code>${2}</code>"
-    -o /dev/null
+        -d text="<b>[$STATUS] </b><code>${1}</code>" \
+        -o /dev/null
 }
 
 send_file() {
@@ -56,21 +57,23 @@ send_file() {
         -F document=@"${2}" \
         -F chat_id="${CHAT_ID}" \
         -F "parse_mode=html" \
-        -F caption="${1}"
-    -o /dev/null
+        -F caption="${1}" \
+        -o /dev/null
 }
 
 # Build ZSTD
 ZSTD_VERSION=v1.5.6
 git clone --depth=1 https://github.com/facebook/zstd -b $ZSTD_VERSION $WORKDIR/zstd
 cd $WORKDIR/zstd
-cmake build/cmake -DCMAKE_INSTALL_PREFIX="$INSTALL_FOLDER/.zstd" |& tee -a $WORKDIR/build.log
-make -j${NPROC} |& tee -a $WORKDIR/build.log
-make install -j${NPROC} |& tee -a $WORKDIR/build.log
+(
+cmake build/cmake -DCMAKE_INSTALL_PREFIX="$INSTALL_FOLDER/.zstd"
+make -j${NPROC}
+make install -j${NPROC}
+) |& tee -a $WORKDIR/build.log
 cd $WORKDIR
 
 # Build LLVM
-send_info "CI : " "Building $VENDOR_STRING clang $STATUS"
+send_info "Building $VENDOR_STRING clang..."
 
 $WORKDIR/build-llvm.py ${ADD} \
     --ref "$TAG" \
@@ -88,9 +91,9 @@ $WORKDIR/build-llvm.py ${ADD} \
 
 # Check LLVM files
 if [[ -f "$INSTALL_FOLDER/bin/clang" ]] || [[ -f "$WORKDIR/build/llvm/instrumented/profdata.prof" ]]; then
-    send_info "CI : " "Compilation finished."
+    send_info "Build finished."
 else
-    send_info "CI : " "Compilation failed."
+    send_info "Build failed."
     send_file "Build log" $WORKDIR/build.log
     exit 1
 fi
@@ -105,7 +108,7 @@ if $FINAL; then
     rm -rf strip .file-idx
 
     # Release
-    send_info "CI : " "Releasing into GitHub..."
+    send_info "Releasing into GitHub..."
     CLANG_VERSION="$($INSTALL_FOLDER/bin/clang --version | head -n1 | cut -d ' ' -f4)"
     MESSAGE="clang ${CLANG_VERSION}-${BUILD_DATE}"
     cd $INSTALL_FOLDER
@@ -113,7 +116,7 @@ if $FINAL; then
     cd $WORKDIR
     git config --global user.name gacorprjkt-bot
     git config --global user.email gacorprjkt-bot@pornhub.com
-    git clone https://ambatubash69:${GITHUB_TOKEN}@github.com/KLC-DEV/gacor-clang -b main $WORKDIR/clang-rel
+    git clone https://linastorvalds:${GITHUB_TOKEN}@github.com/linastorvalds/gacorprjkt-clang $WORKDIR/clang-rel
     cd $WORKDIR/clang-rel
     cat dummy |
         sed "s/LLVM_VERSION/${CLANG_VERSION}-${BUILD_DATE}/g" |
@@ -123,6 +126,6 @@ if $FINAL; then
     git push origin main || exit 1
     mv $INSTALL_FOLDER/clang.tar.zst .
     hub release create -a clang.tar.zst -m "${MESSAGE}" ${BUILD_TAG}
-    send_info "CI : " "Toolchain released."
+    send_info "Toolchain released."
     cd $WORKDIR
 fi
