14b90ff3bba62d877735e4ddb00d6bb79790ddea
gacorprjkt-admin <you@example.com>
2025-01-02 09:12:56 +0800

    changes

--- ci.sh
+++ ci.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/env bash
 
-export LLVM_NAME="kucing"
-export STABLE_TAG="llvmorg-19.1.6"
-export INSTALL="${PWD}/install"
-export CHAT_ID="-1001180467256"
-export BUILD_DATE="$(date +%Y%m%d)"
-export BUILD_DAY="$(date "+%d %B %Y")"
-export BUILD_TAG="$(date +%Y%m%d-%H%M-%Z)"
-export NPROC="$(echo $(($(nproc --all) * 2)))"
-export CUSTOM_FLAGS="
+if [[ -z $TELEGRAM_TOKEN ]]; then
+    echo "Please add TELEGRAM_TOKEN secret!"
+    exit 1
+fi
+
+if [[ -z $GITHUB_TOKEN ]]; then
+    echo "Please add GITHUB_TOKEN secret!"
+    exit 1
+fi
+
+export WORKDIR=$(pwd)
+export VENDOR_STRING=gacorprjkt
+export TAG=llvmorg-19.1.6
+export INSTALL_FOLDER=$WORKDIR/install
+export CHAT_ID=-1002254721074
+export BUILD_DATE=$(date +%Y%m%d)
+export BUILD_TAG=$(date +%Y%m%d-%H%M-%Z)
+export NPROC=$(($(nproc --all) * 2))
+export FLAGS="
   LLVM_PARALLEL_TABLEGEN_JOBS=${NPROC}
   LLVM_PARALLEL_COMPILE_JOBS=${NPROC}
   LLVM_PARALLEL_LINK_JOBS=${NPROC}
@@ -21,129 +31,95 @@ export CUSTOM_FLAGS="
   CMAKE_STATIC_LINKER_FLAGS='-O3 --lto-O3 --lto-CGO3 --gc-sections --strip-debug'
   "
 
-FINAL=false
-RELEASE=false
-for ARGS in $@; do
-  case $ARGS in
-  final)
-    FINAL=true
-    ;;
-  release)
-    RELEASE=true
-    ;;
-  esac
-done
-export FINAL RELEASE
+export FINAL=false
+if [[ $1 == "final" ]]; then
+    export FINAL=true
+fi
 
 send_info() {
-  curl -s -X POST https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage \
-    -d chat_id="${CHAT_ID}" \
-    -d "parse_mode=html" \
-    -d text="<b>${1}</b><code>${2}</code>" >/dev/null 2>&1
+    curl -s -X POST https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage \
+        -d chat_id="${CHAT_ID}" \
+        -d "parse_mode=html" \
+        -d text="<b>${1}</b><code>${2}</code>"
+    -o /dev/null
 }
 
 send_file() {
-  curl -s -X POST https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendDocument \
-    -F document=@"${2}" \
-    -F chat_id="${CHAT_ID}" \
-    -F "parse_mode=html" \
-    -F caption="${1}" >/dev/null 2>&1
+    curl -s -X POST https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendDocument \
+        -F document=@"${2}" \
+        -F chat_id="${CHAT_ID}" \
+        -F "parse_mode=html" \
+        -F caption="${1}"
+    -o /dev/null
 }
 
-build_llvm() {
-  send_info "GitHub Action : " "Building LLVM . . ."
-  BUILD_START=$(date +"%s")
+# Build ZSTD
+git clone https://github.com/facebook/zstd --depth=1 $WORKDIR/zstd
+cd $WORKDIR/zstd
+ZSTD_VERSION=$(git describe --abbrev=0 --tags)
+git switch $ZSTD_VERSION
+cmake build/cmake -DCMAKE_INSTALL_PREFIX="$INSTALL_FOLDER/.zstd" |& tee -a $WORKDIR/build.log
+make -j${NPROC} |& tee -a $WORKDIR/build.log
+make install -j${NPROC} |& tee -a $WORKDIR/build.log
+cd $WORKDIR
 
-  if ${FINAL}; then
-    ADD="${ADD} --final"
-  fi
+# Build LLVM
+send_info "CI : " "Building $VENDOR_STRING clang . . ."
 
-  if ${RELEASE}; then
-    ADD="${ADD} --ref ${STABLE_TAG}"
-  fi
+if ${FINAL}; then
+    ADD="--final"
+fi
 
-  ./build-llvm.py ${ADD} \
+$WORKDIR/build-llvm.py ${ADD} \
+    --ref "$TAG" \
     --build-type "Release" \
     --build-stage1-only \
-    --defines "${CUSTOM_FLAGS}" \
-    --install-folder "${INSTALL}" \
+    --defines "$FLAGS" \
+    --install-folder "$INSTALL_FOLDER" \
     --lto thin \
     --pgo llvm \
     --projects clang lld polly \
     --shallow-clone \
     --targets AArch64 X86 \
     --no-update \
-    --vendor-string "${LLVM_NAME}" |& tee -a build.log
-  BUILD_END=$(date +"%s")
-  DIFF=$((BUILD_END - BUILD_START))
+    --vendor-string "$VENDOR_STRING" |& tee -a $WORKDIR/build.log
 
-  # Check LLVM files
-  if [ -f ${INSTALL}/bin/clang ] || [ -f ${PWD}/build/llvm/instrumented/profdata.prof ]; then
-    send_info "GitHub Action : " "LLVM compilation finished ! ! !"
-    send_info "Time taken : " "$((DIFF / 60))m $((DIFF % 60))s"
-  else
-    send_info "GitHub Action : " "LLVM compilation failed ! ! !"
-    send_file "LLVM build.log" ./build.log
+# Check LLVM files
+if [[ -f "$INSTALL_FOLDER/bin/clang" ]]; then
+    send_info "CI : " "Compilation finished."
+    send_file "Build log" $WORKDIR/build.log
+else
+    send_info "CI : " "Compilation failed."
+    send_file "Build log" $WORKDIR/build.log
     exit 1
-  fi
-}
-
-build_zstd() {
-  git clone https://github.com/facebook/zstd -b v1.5.6 --depth=1
-  cd zstd
-  cmake build/cmake -DCMAKE_INSTALL_PREFIX="${INSTALL}/.zstd" |& tee -a build.log
-  make -j${NPROC} |& tee -a build.log
-  make install -j${NPROC} |& tee -a build.log
-  cd -
-}
-
-strip_binaries() {
-  find ${INSTALL} -type f -exec file {} \; >.file-idx
-  cp ${INSTALL}/bin/llvm-objcopy ./strip
-  grep "not strip" .file-idx |
-    tr ':' ' ' | awk '{print $1}' |
-    while read -r file; do ./strip --strip-all-gnu "${file}"; done
-
-  # clean unused files
-  rm -rf strip .file-idx
-}
-
-git_release() {
-  CLANG_VERSION="$(${INSTALL}/bin/clang --version | head -n1 | cut -d ' ' -f4)"
-  MESSAGE="Clang: ${CLANG_VERSION}-${BUILD_DATE}"
-  send_info "GitHub Action : " "Release into GitHub . . ."
-  send_info "Clang Version : " "${CLANG_VERSION}"
-  cd ${INSTALL}
-  tar -I"${INSTALL}/.zstd/bin/zstd --ultra -22 -T0" -cf clang.tar.zst *
-  cd ..
-  git config --global user.name github-actions[bot]
-  git config --global user.email github-actions[bot]@users.noreply.github.com
-  if ${RELEASE}; then
-    git clone https://Diaz1401:${GITHUB_TOKEN}@github.com/mengkernel/clang-stable.git clang -b main
-  else
-    git clone https://Diaz1401:${GITHUB_TOKEN}@github.com/mengkernel/clang.git clang -b main
-  fi
-  cd clang
-  cat README |
-    sed s/LLVM_VERSION/${CLANG_VERSION}-${BUILD_DATE}/g |
-    sed s/SIZE/$(du -m ${INSTALL}/clang.tar.zst | cut -f1)/g >README.md
-  git commit --allow-empty -as -m "${MESSAGE}"
-  git push origin main
-  cp ${INSTALL}/clang.tar.zst .
-  hub release create -a clang.tar.zst -m "${MESSAGE}" ${BUILD_TAG}
-  send_info "GitHub Action : " "Toolchain released ! ! !"
-  cd ..
-}
-
-TOTAL_START=$(date +"%s")
-send_info "Date : " "${BUILD_DAY}"
-send_info "GitHub Action : " "Toolchain compilation started . . ."
-build_zstd
-build_llvm
-if ${FINAL}; then
-  strip_binaries
-  git_release
 fi
-TOTAL_END=$(date +"%s")
-DIFF=$((TOTAL_END - TOTAL_START))
-send_info "Total CI operation : " "$((DIFF / 60))m $((DIFF % 60))"
+
+# Strip binaries
+OBJCOPY=$INSTALL_FOLDER/bin/llvm-objcopy
+find "$INSTALL_FOLDER" -type f -exec file {} \; >.file-idx
+grep "not strip" .file-idx | tr ':' ' ' | awk '{print $1}' | while read -r file; do
+    $OBJCOPY --strip-all-gnu "$file"
+done
+rm -rf strip .file-idx
+
+# Release
+send_info "CI : " "Releasing into GitHub..."
+CLANG_VERSION="$($INSTALL_FOLDER/bin/clang --version | head -n1 | cut -d ' ' -f4)"
+MESSAGE="clang ${CLANG_VERSION}-${BUILD_DATE}"
+cd $INSTALL_FOLDER
+tar -I"$INSTALL_FOLDER/.zstd/bin/zstd --ultra -22 -T0" -cf clang.tar.zst *
+cd $WORKDIR
+git config --global user.name gacorprjkt-bot
+git config --global user.email gacorprjkt-bot@pornhub.com
+git clone https://ambatubash69:${GITHUB_TOKEN}@github.com/KLC-DEV/gacor-clang -b main $WORKDIR/clang-rel
+cd $WORKDIR/clang-rel
+cat README |
+    sed "s/LLVM_VERSION/${CLANG_VERSION}-${BUILD_DATE}/g" |
+    sed "s/SIZE_MB/$(du -m $INSTALL_FOLDER/clang.tar.zst | cut -f1)/g" |
+    sed "s/ZSTD_VERSION/${ZSTD_VERSION}/g" >README.md
+git commit --allow-empty -as -m "${MESSAGE}"
+git push origin main || exit 1
+mv $INSTALL_FOLDER/clang.tar.zst .
+hub release create -a clang.tar.zst -m "${MESSAGE}" ${BUILD_TAG}
+send_info "CI : " "Toolchain released."
+cd $WORKDIR
