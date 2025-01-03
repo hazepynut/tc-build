63f75201c20dc5f57e3e5dc4f64d2f15d0543fbd
gacorprjkt-admin <you@example.com>
2025-01-03 18:12:56 +0800

    treewide: major changes

---
+++ build_tc.sh
@@ -0,0 +1,96 @@
+#!/usr/bin/env bash
+
+if [[ -z $GITHUB_TOKEN ]]; then
+    echo "[ERROR] Missing GitHub Token!"
+    exit 1
+fi
+
+WORKDIR=$(pwd)
+TAG=llvmorg-19.1.6
+INSTALL_FOLDER=$WORKDIR/install
+BUILD_DATE=$(date +%Y%m%d)
+BUILD_TAG=$(date +%Y%m%d-%H%M)
+NPROC=$(($(nproc --all) * 2))
+FLAGS="
+  LLVM_PARALLEL_COMPILE_JOBS=${NPROC}
+  LLVM_PARALLEL_LINK_JOBS=${NPROC}
+  CMAKE_C_FLAGS='-O3'
+  CMAKE_CXX_FLAGS='-O3'
+  "
+
+if [[ $1 == "final" ]]; then
+    FINAL=true
+    ADD="--final"
+else
+    FINAL=false
+fi
+
+# Build ZSTD
+ZSTD_VERSION=v1.5.6
+git clone --depth=1 https://github.com/facebook/zstd -b $ZSTD_VERSION $WORKDIR/zstd
+cd $WORKDIR/zstd
+cmake build/cmake -DCMAKE_INSTALL_PREFIX="$INSTALL_FOLDER/.zstd"
+make -j${NPROC}
+make install -j${NPROC}
+cd $WORKDIR
+
+# Build LLVM
+$WORKDIR/build-llvm.py ${ADD} \
+    --bolt \
+    --defines "$FLAGS" \
+    --install-folder "$INSTALL_FOLDER" \
+    --lto thin \
+    --no-update \
+    --pgo llvm \
+    --projects clang lld polly \
+    --quiet-cmake \
+    --ref "$TAG" \
+    --shallow-clone \
+    --targets AArch64 \
+    --vendor-string "gacorprjkt"
+
+# Check LLVM files
+if [[ -f "$INSTALL_FOLDER/bin/clang" ]] || [[ -f "$WORKDIR/build/llvm/instrumented/profdata.prof" ]]; then
+    :
+else
+    echo "[ERROR] Build LLVM Failed"
+    exit 1
+fi
+
+if $FINAL; then
+    # Build Binutils
+    $WORKDIR/build-binutils.py \
+        --targets aarch64 arm \
+        --install-folder "$INSTALL_FOLDER"
+
+    # Strip binaries
+    OBJCOPY=$INSTALL_FOLDER/bin/llvm-objcopy
+    find "$INSTALL_FOLDER" -type f -exec file {} \; >.file-idx
+    grep "not strip" .file-idx | tr ':' ' ' | awk '{print $1}' | while read -r file; do
+        $OBJCOPY --strip-all-gnu "$file"
+    done
+    rm -rf strip .file-idx
+
+    # Release
+    CLANG_VERSION="$($INSTALL_FOLDER/bin/clang --version | head -n1 | cut -d ' ' -f4)"
+    BINUTILS_VERSION=$($INSTALL_FOLDER/bin/aarch64-linux-gnu-ld --version | head -n1 | grep -o '[0-9].*')
+    MESSAGE="clang ${CLANG_VERSION}-${BUILD_DATE}"
+    cd $INSTALL_FOLDER
+    tar -I"$INSTALL_FOLDER/.zstd/bin/zstd --ultra -22 -T0" -cf clang.tar.zst *
+    cd $WORKDIR
+    git config --global user.name gacorprjkt-bot
+    git config --global user.email gacorprjkt-bot@pornhub.com
+    git clone https://Asteroidd21:${GITHUB_TOKEN}@github.com/Asteroidd21/gacorprjkt-clang $WORKDIR/clang-rel
+    cd $WORKDIR/clang-rel
+    cat dummy |
+        sed "s/LLVM_VERSION/${CLANG_VERSION} (${BUILD_DATE})/g" |
+        sed "s/SIZE_MB/$(du -m $INSTALL_FOLDER/clang.tar.zst | cut -f1)/g" |
+        sed "s/ZSTD_VERSION/${ZSTD_VERSION}/g" |
+        sed "s/BINUTILS_VERSION/${BINUTILS_VERSION}/g" >README.md
+    git add .
+    git commit -m "${MESSAGE}"
+    git push origin main || exit 1
+    mv $INSTALL_FOLDER/clang.tar.zst .
+    hub release create -a clang.tar.zst -m "${MESSAGE}" ${BUILD_TAG}
+    cd $WORKDIR
+fi
--- ci.sh
+++ ci.sh
@@ -1,131 +1,112 @@
 #!/usr/bin/env bash
 
-if [[ -z $TELEGRAM_TOKEN ]]; then
-    echo "Please add TELEGRAM_TOKEN secret!"
-    exit 1
-fi
+base=$(dirname "$(readlink -f "$0")")
+install=$base/install
+src=$base/src
 
-if [[ -z $GITHUB_TOKEN ]]; then
-    echo "Please add GITHUB_TOKEN secret!"
-    exit 1
-fi
+set -eu
 
-export WORKDIR=$(pwd)
-export VENDOR_STRING=gacorprjkt
-export TAG=llvmorg-19.1.6
-export INSTALL_FOLDER=$WORKDIR/install
-export CHAT_ID=-1002254721074
-export BUILD_DATE=$(date +%Y%m%d)
-export BUILD_TAG=$(date +%Y%m%d-%H%M-%Z)
-export NPROC=$(($(nproc --all) * 2))
-export FLAGS="
-  LLVM_PARALLEL_TABLEGEN_JOBS=${NPROC}
-  LLVM_PARALLEL_COMPILE_JOBS=${NPROC}
-  LLVM_PARALLEL_LINK_JOBS=${NPROC}
-  LLVM_OPTIMIZED_TABLEGEN=ON
-  CMAKE_C_FLAGS='-O3 -pipe -ffunction-sections -fdata-sections -fno-plt -fmerge-all-constants -fomit-frame-pointer -funroll-loops -falign-functions=64 -march=skylake -mtune=skylake -mllvm -polly -mllvm -polly-position=early -mllvm -polly-vectorizer=stripmine -mllvm -polly-run-dce'
-  CMAKE_CXX_FLAGS='-O3 -pipe -ffunction-sections -fdata-sections -fno-plt -fmerge-all-constants -fomit-frame-pointer -funroll-loops -falign-functions=64 -march=skylake -mtune=skylake -mllvm -polly -mllvm -polly-position=early -mllvm -polly-vectorizer=stripmine -mllvm -polly-run-dce'
-  CMAKE_EXE_LINKER_FLAGS='-O3 --lto-O3 --lto-CGO3 --gc-sections --strip-debug'
-  CMAKE_MODULE_LINKER_FLAGS='-O3 --lto-O3 --lto-CGO3 --gc-sections --strip-debug'
-  CMAKE_SHARED_LINKER_FLAGS='-O3 --lto-O3 --lto-CGO3 --gc-sections --strip-debug'
-  CMAKE_STATIC_LINKER_FLAGS='-O3 --lto-O3 --lto-CGO3 --gc-sections --strip-debug'
-  "
-
-if [[ $1 == "final" ]]; then
-    export FINAL=true
-else
-    export FINAL=false
-fi
-
-if $FINAL; then
-    STATUS="FINAL"
-    ADD="--final"
-else
-    STATUS="PROF"
-fi
-
-send_info() {
-    curl -s -X POST https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage \
-        -d chat_id="${CHAT_ID}" \
-        -d "parse_mode=html" \
-        -d text="<b>[$STATUS] </b><code>${1}</code>" \
-        -o /dev/null
-}
-
-send_file() {
-    curl -s -X POST https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendDocument \
-        -F document=@"${2}" \
-        -F chat_id="${CHAT_ID}" \
-        -F "parse_mode=html" \
-        -F caption="${1}" \
-        -o /dev/null
-}
-
-# Build ZSTD
-ZSTD_VERSION=v1.5.6
-git clone --depth=1 https://github.com/facebook/zstd -b $ZSTD_VERSION $WORKDIR/zstd
-cd $WORKDIR/zstd
-(
-cmake build/cmake -DCMAKE_INSTALL_PREFIX="$INSTALL_FOLDER/.zstd"
-make -j${NPROC}
-make install -j${NPROC}
-) |& tee -a $WORKDIR/build.log
-cd $WORKDIR
-
-# Build LLVM
-send_info "Building $VENDOR_STRING clang..."
-
-$WORKDIR/build-llvm.py ${ADD} \
-    --ref "$TAG" \
-    --build-type "Release" \
-    --build-stage1-only \
-    --defines "$FLAGS" \
-    --install-folder "$INSTALL_FOLDER" \
-    --lto thin \
-    --pgo llvm \
-    --projects clang lld polly \
-    --shallow-clone \
-    --targets AArch64 X86 \
-    --no-update \
-    --vendor-string "$VENDOR_STRING" |& tee -a $WORKDIR/build.log
-
-# Check LLVM files
-if [[ -f "$INSTALL_FOLDER/bin/clang" ]] || [[ -f "$WORKDIR/build/llvm/instrumented/profdata.prof" ]]; then
-    send_info "Build finished."
-else
-    send_info "Build failed."
-    send_file "Build log" $WORKDIR/build.log
-    exit 1
-fi
-
-if $FINAL; then
-    # Strip binaries
-    OBJCOPY=$INSTALL_FOLDER/bin/llvm-objcopy
-    find "$INSTALL_FOLDER" -type f -exec file {} \; >.file-idx
-    grep "not strip" .file-idx | tr ':' ' ' | awk '{print $1}' | while read -r file; do
-        $OBJCOPY --strip-all-gnu "$file"
+function parse_parameters() {
+    while (($#)); do
+        case $1 in
+            all | binutils | deps | kernel | llvm) action=$1 ;;
+            *) exit 33 ;;
+        esac
+        shift
     done
-    rm -rf strip .file-idx
+}
 
-    # Release
-    send_info "Releasing into GitHub..."
-    CLANG_VERSION="$($INSTALL_FOLDER/bin/clang --version | head -n1 | cut -d ' ' -f4)"
-    MESSAGE="clang ${CLANG_VERSION}-${BUILD_DATE}"
-    cd $INSTALL_FOLDER
-    tar -I"$INSTALL_FOLDER/.zstd/bin/zstd --ultra -22 -T0" -cf clang.tar.zst *
-    cd $WORKDIR
-    git config --global user.name gacorprjkt-bot
-    git config --global user.email gacorprjkt-bot@pornhub.com
-    git clone https://Asteroidd21:${GITHUB_TOKEN}@github.com/Asteroidd21/gacorprjkt-clang $WORKDIR/clang-rel
-    cd $WORKDIR/clang-rel
-    cat dummy |
-        sed "s/LLVM_VERSION/${CLANG_VERSION}-${BUILD_DATE}/g" |
-        sed "s/SIZE_MB/$(du -m $INSTALL_FOLDER/clang.tar.zst | cut -f1)/g" |
-        sed "s/ZSTD_VERSION/${ZSTD_VERSION}/g" >README.md
-    git commit --allow-empty -as -m "${MESSAGE}"
-    git push origin main || exit 1
-    mv $INSTALL_FOLDER/clang.tar.zst .
-    hub release create -a clang.tar.zst -m "${MESSAGE}" ${BUILD_TAG}
-    send_info "Toolchain released."
-    cd $WORKDIR
-fi
+function do_all() {
+    do_deps
+    do_llvm
+    do_binutils
+    do_kernel
+}
+
+function do_binutils() {
+    "$base"/build-binutils.py \
+        --install-folder "$install" \
+        --show-build-commands \
+        --targets x86_64
+}
+
+function do_deps() {
+    # We only run this when running on GitHub Actions
+    [[ -z ${GITHUB_ACTIONS:-} ]] && return 0
+
+    sudo apt-get install -y --no-install-recommends \
+        bc \
+        bison \
+        ca-certificates \
+        clang \
+        cmake \
+        curl \
+        file \
+        flex \
+        gcc \
+        g++ \
+        git \
+        libelf-dev \
+        libssl-dev \
+        lld \
+        make \
+        ninja-build \
+        python3 \
+        texinfo \
+        xz-utils \
+        zlib1g-dev
+}
+
+function do_kernel() {
+    local branch=linux-rolling-stable
+    local linux=$src/$branch
+
+    if [[ -d $linux ]]; then
+        git -C "$linux" fetch --depth=1 origin $branch
+        git -C "$linux" reset --hard FETCH_HEAD
+    else
+        git clone \
+            --branch "$branch" \
+            --depth=1 \
+            --single-branch \
+            https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git \
+            "$linux"
+    fi
+
+    cat <<EOF | env PYTHONPATH="$base"/tc_build python3 -
+from pathlib import Path
+
+from kernel import LLVMKernelBuilder
+
+builder = LLVMKernelBuilder()
+builder.folders.build = Path('$base/build/linux')
+builder.folders.source = Path('$linux')
+builder.matrix = {'defconfig': ['X86']}
+builder.toolchain_prefix = Path('$install')
+
+builder.build()
+EOF
+}
+
+function do_llvm() {
+    extra_args=()
+    [[ -n ${GITHUB_ACTIONS:-} ]] && extra_args+=(--no-ccache)
+
+    "$base"/build-llvm.py \
+        --assertions \
+        --build-stage1-only \
+        --build-target distribution \
+        --check-targets clang lld llvm \
+        --install-folder "$install" \
+        --install-target distribution \
+        --projects clang lld \
+        --quiet-cmake \
+        --ref release/19.x \
+        --shallow-clone \
+        --show-build-commands \
+        --targets X86 \
+        "${extra_args[@]}"
+}
+
+parse_parameters "$@"
+do_"${action:=all}"
