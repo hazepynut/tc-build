17f32c32d5a035e5ad1cec15feded1a1eb917cc1
gacorprjkt-admin <you@example.com>
2025-01-05 13:07:00 +0800

    build_tc.sh: Update

--- build_tc.sh
+++ build_tc.sh
@@ -6,8 +6,10 @@ if [[ -z $GITHUB_TOKEN ]]; then
 fi
 
 WORKDIR=$(pwd)
-TAG=llvmorg-19.1.6
+LLVM_VERSION=19.1.6
+ZSTD_VERSION=v1.5.6
 INSTALL_FOLDER=$WORKDIR/install
+CUSTOM_FLAGS="LLVM_PARALLEL_TABLEGEN_JOBS=${NPROC} LLVM_PARALLEL_COMPILE_JOBS=${NPROC} LLVM_PARALLEL_LINK_JOBS=${NPROC} LLVM_OPTIMIZED_TABLEGEN=ON CMAKE_C_FLAGS='-g0 -O3' CMAKE_CXX_FLAGS='-g0 -O3' CMAKE_EXE_LINKER_FLAGS='-g0 -O3' CMAKE_MODULE_LINKER_FLAGS='-g0 -O3' CMAKE_SHARED_LINKER_FLAGS='-g0 -O3' CMAKE_STATIC_LINKER_FLAGS='-g0 -O3'"
 BUILD_DATE=$(date +%Y%m%d)
 BUILD_TAG=$(date +%Y%m%d-%H%M)
 NPROC=$(nproc --all)
@@ -20,7 +22,6 @@ else
 fi
 
 # Build ZSTD
-ZSTD_VERSION=v1.5.6
 git clone --depth=1 https://github.com/facebook/zstd -b $ZSTD_VERSION $WORKDIR/zstd
 cd $WORKDIR/zstd
 cmake build/cmake -DCMAKE_INSTALL_PREFIX="$INSTALL_FOLDER/.zstd"
@@ -32,13 +33,13 @@ cd $WORKDIR
 $WORKDIR/build-llvm.py $ADD \
     --build-type "Release" \
     --build-stage1-only \
-    --defines "LLVM_PARALLEL_TABLEGEN_JOBS=${NPROC} LLVM_PARALLEL_COMPILE_JOBS=${NPROC} LLVM_PARALLEL_LINK_JOBS=${NPROC} LLVM_OPTIMIZED_TABLEGEN=ON CMAKE_C_FLAGS='-O3' CMAKE_CXX_FLAGS='-O3' CMAKE_EXE_LINKER_FLAGS='-O3' CMAKE_MODULE_LINKER_FLAGS='-O3' CMAKE_SHARED_LINKER_FLAGS='-O3' CMAKE_STATIC_LINKER_FLAGS='-O3'" \
+    --defines "$CUSTOM_FLAGS" \
     --install-folder "$INSTALL_FOLDER" \
     --lto thin \
     --pgo llvm \
     --projects clang lld polly \
     --quiet-cmake \
-    --ref "$TAG" \
+    --ref "llvmorg-$LLVM_VERSION" \
     --shallow-clone \
     --targets AArch64 ARM X86 \
     --no-update \
@@ -66,25 +67,35 @@ if $FINAL; then
     done
     rm -rf strip .file-idx
 
-    # Release
+    ## Release
     CLANG_VERSION="$($INSTALL_FOLDER/bin/clang --version | head -n1 | cut -d ' ' -f4)"
     BINUTILS_VERSION=$($INSTALL_FOLDER/bin/aarch64-linux-gnu-ld --version | head -n1 | grep -o '[0-9].*')
+    ZSTD_VERSION=$(echo "$ZSTD_VERSION" | tr -d 'v')
+    GLIBC_VERSION=$(ldd --version | head -n1 | grep -oE '[^ ]+$')
     MESSAGE="clang ${CLANG_VERSION}-${BUILD_DATE}"
+    
+    # Compress
     cd $INSTALL_FOLDER
     tar -I"$INSTALL_FOLDER/.zstd/bin/zstd --ultra -22 -T0" -cf clang.tar.zst *
+    ARCHIVE_SIZE=$(du -m clang.tar.zst | cut -f1)
     cd $WORKDIR
+    
+    # Set README
     git config --global user.name gacorprjkt-bot
     git config --global user.email gacorprjkt-bot@pornhub.com
     git clone https://Asteroidd21:${GITHUB_TOKEN}@github.com/Asteroidd21/gacorprjkt-clang $WORKDIR/clang-rel
     cd $WORKDIR/clang-rel
     cat dummy |
         sed "s/LLVM_VERSION/${CLANG_VERSION} (${BUILD_DATE})/g" |
-        sed "s/SIZE_MB/$(du -m $INSTALL_FOLDER/clang.tar.zst | cut -f1)/g" |
+        sed "s/ARCHIVE_SIZE/${ARCHIVE_SIZE}MB/g" |
         sed "s/ZSTD_VERSION/${ZSTD_VERSION}/g" |
-        sed "s/BINUTILS_VERSION/${BINUTILS_VERSION}/g" >README.md
+        sed "s/BINUTILS_VERSION/${BINUTILS_VERSION}/g" |
+        sed "s/GLIBC_VERSION/${GLIBC_VERSION}/g" >README.md
     git add .
     git commit -m "${MESSAGE}"
     git push origin main || exit 1
+    
+    # Upload Archive
     mv $INSTALL_FOLDER/clang.tar.zst .
     hub release create -a clang.tar.zst -m "${MESSAGE}" ${BUILD_TAG}
     cd $WORKDIR
